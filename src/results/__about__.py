4059e6f3a8791970a784f05302c3d2beb9451add
charbonnierg <guillaume.charbonnier@araymond.com>
2023-04-17 23:12:49 +0200

    chore(project): initialize project layout and configured development tools

---
+++ src/results/__about__.py
@@ -0,0 +1,19 @@
+"""The `__about__` module exposes the version of the `results` package:
+
+Example:
+
+```python
+from results.__about__ import __version__
+print(__version__)
+```
+
+Note that container version can also be imported directly from `results` package:
+
+Example:
+
+```python
+from results import __version__
+print(__version__)
+```
+"""
+__version__ = "0.1.0"
